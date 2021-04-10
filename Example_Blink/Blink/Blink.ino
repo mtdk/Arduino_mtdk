b00b75d2254a5cee73025416f09762c2a420820b
mtdk <ruixiangxie@outlook.com>
2021-04-11 00:58:36 +0800

    202104110058

--- Example_Blink/Blink/Blink.ino
+++ Example_Blink/Blink/Blink.ino
@@ -25,13 +25,22 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(13, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);   // 设置LED灯高电位(亮起)500ms
+  delay(500);                       // 等待500ms
+  digitalWrite(LED_BUILTIN, LOW);    // 设置LED灯低电位(关闭)300ms
+  delay(300);                       // 等待300ms
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+
 }
